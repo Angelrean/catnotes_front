5fcb894345f3f432330787f2d47648b26b19ab98
Angelrean <angelrean02@gmail.com>
2024-09-23 13:19:30 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyectoTareas',
+  webDir: 'www'
+};
+
+export default config;
