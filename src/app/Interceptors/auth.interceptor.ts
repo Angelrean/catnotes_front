56a962b3923765a676e66279035d3d304dd393ed
Aratt-Gaytan <aratt.gaytan.flores0@gmail.com>
2024-11-02 16:03:35 -0600

    correccion de errores

--- src/app/Interceptors/auth.interceptor.ts
+++ src/app/Interceptors/auth.interceptor.ts
@@ -9,14 +9,19 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private readonly coockieService: CookieService){}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.coockieService.getToken();
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
+    console.log(request);
+    if (!request.url.includes("google.com")) {
+      const token = this.coockieService.getToken();
+      if (token) {
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+      }
+      // Aquí puedes realizar alguna otra acción, como mostrar un mensaje al usuario, enviar datos, etc.
     }
+
     return next.handle(request);
   }
 }
